cd6d540bbea530133c5a81861594eba2d81f3508
odinets <odinets@irc.beltelecom.by>
2020-08-18 15:52:56 +0300

    first commit

---
+++ src/by/odinets/codewars/simpleTimeDifference/Runner.java
@@ -0,0 +1,9 @@
+package by.odinets.codewars.simpleTimeDifference;
+
+public class Runner {
+
+	public void main(String[] args) {
+		String[] arr = {"14:51"};
+		Solution.solve(arr);
+	}
+}
---
+++ src/by/odinets/codewars/simpleTimeDifference/Solution.java
@@ -0,0 +1,10 @@
+package by.odinets.codewars.simpleTimeDifference;
+
+public class Solution {
+	public static String solve(String [] arr){
+		for(String elem : arr) {
+			System.out.println("elem :: " + elem);
+		}
+		return "";
+	}
+}
