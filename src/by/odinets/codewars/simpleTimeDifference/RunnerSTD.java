99edc3676932d78a28318c68c543ac1113f55151
odinets <odinets@irc.beltelecom.by>
2020-08-19 14:31:20 +0300

    commit3

--- src/by/odinets/codewars/simpleTimeDifference/RunnerSTD.java
+++ src/by/odinets/codewars/simpleTimeDifference/RunnerSTD.java
@@ -2,9 +2,16 @@ package by.odinets.codewars.simpleTimeDifference;
 
 public class RunnerSTD {
 
-	public void main(String[] args) {
-		String[] arr = {"14:51"};
-		Solution.solve(arr);
+	public static void main(String[] args) {
+		String[] arr = {"14.51"};
+		String[] arr1 = {"23:00", "04:22", "18:05", "06:24"};
+		String example1 = Solution.solve(arr);
+		String example2 = Solution.solve(arr1);
+		System.out.println(" example1 :: " + example1);
+		System.out.println(" example2 :: " + example2);
 		
+		String[] arr2 = {"21:14", "15:34", "14:51", "06:25", "15:30"};
+		String example3 = Solution.solve(arr2);
+		System.out.println(" example3 :: " + example3);
 	}
 }
--- src/by/odinets/codewars/simpleTimeDifference/Solution.java
+++ src/by/odinets/codewars/simpleTimeDifference/Solution.java
@@ -1,10 +1,70 @@
 package by.odinets.codewars.simpleTimeDifference;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Solution {
+	
 	public static String solve(String [] arr){
+		Set<Integer> allInterval = new TreeSet<Integer>();
+		allInterval = convertToMinute(arr);
+		int maxInterval = searchMaxInterval(allInterval);
+		String maxIntervalStr = convertToHourMinute(maxInterval);
+		return maxIntervalStr;
+	}
+	
+	public static Set<Integer> convertToMinute(String[] arr) {
+		Set<Integer> allInterval = new TreeSet<Integer>();
 		for(String elem : arr) {
-			System.out.println("elem :: " + elem);
+			String hours = "";
+			String min = "";
+			if(elem.contains(":")) {
+				hours = elem.split(":")[0];
+				min = elem.split(":")[1];
+			}
+			if(elem.contains(".")) {
+				hours = elem.split("\\.")[0];
+				min = elem.split("\\.")[1];
+			}
+			int hoursInt = Integer.parseInt(hours);
+			int minInt = Integer.parseInt(min);
+			int hoursToMin = hoursInt*60;
+			int minAll = hoursToMin + minInt;
+			allInterval.add(minAll);
 		}
-		return "";
+		System.out.println("allInterval :: " + allInterval);
+		return allInterval;
+	}
+	
+	public static Integer searchMaxInterval(Set<Integer> allInterval) {
+		int maxInterval = 0;
+		int firstElement = allInterval.iterator().next();
+		for(Integer elem : allInterval) {
+			int interval = elem - firstElement;
+			if(interval >= maxInterval) {
+				maxInterval = interval;
+			}
+			firstElement = elem;
+		}
+		if(maxInterval == 0) {
+			maxInterval = 24 * 60;
+		}
+		maxInterval = maxInterval - 1;
+		return maxInterval;
+	}
+	
+	public static String convertToHourMinute(Integer maxInterval) {
+		int hours = maxInterval / 60;
+		int minute = maxInterval % 60;
+		String hoursStr = String.valueOf(hours);
+		String minuteStr = String.valueOf(minute);
+		if(hoursStr.length() < 2) {
+			hoursStr = "0" + hoursStr;
+		}
+		if(minuteStr.length() < 2) {
+			minuteStr = "0" + minuteStr;
+		}
+		String hourMinute = hoursStr + ":" + minuteStr;
+		return hourMinute;
 	}
 }
